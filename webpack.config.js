72bf96fb7d0efa22de3bc6453a165f0e9ada860e
Franck Royer <franck@status.im>
2021-10-11 15:03:13 +1100

    wip: Add minified umd file to package

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+module.exports = {
+  mode: 'production',
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [{
+      test: /\.ts$/,
+      use: 'ts-loader',
+      exclude: /node_modules/
+    }]
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+    fallback: { "crypto": false }
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+};
